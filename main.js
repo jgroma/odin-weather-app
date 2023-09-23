bacb4da1194698c8eecb3225f1e01d2fcf22fa17
aspen <jgromadzinska9@gmail.com>
2023-09-23 19:37:25 +0100

    Fetch API data

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Test");
+(()=>{"use strict";console.log("Test"),function(){let o=fetch("http://api.weatherapi.com/v1/current.json?key=90d680eb454742b8a73160825232309&q=London",{mode:"cors"}).then((function(o){console.log(o.json())})).catch((function(o){console.log("Something went wrong.")}));console.log(o)}(),console.log("This is data file")})();
