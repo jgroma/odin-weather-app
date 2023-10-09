6906547bad78c369a9fb38907937915996441a33
aspen <jgromadzinska9@gmail.com>
2023-10-09 19:15:26 +0100

    Change API call to HTTPS

--- src/modules/search.js
+++ src/modules/search.js
@@ -3,7 +3,7 @@ async function fetchData() {
 
     try {
 
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=90d680eb454742b8a73160825232309&q=${searchLocation}&days=3`, 
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=90d680eb454742b8a73160825232309&q=${searchLocation}&days=3`, 
     {mode: 'cors'});
 
     const weatherData = await response.json();
